1031029687b0a8836e48b562bfba6de91ae5aeab
afifalhauzan <afifalhauzan2@gmail.com>
2024-12-03 11:53:55 +0700

    breeze implementasi login dan logout di navbar done, lancar

--- resources/views/lecturers/create.blade.php
+++ resources/views/lecturers/create.blade.php
@@ -22,6 +22,15 @@
                     </li>
                 </ul>
             </div>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
         </div>
     </nav>
 
--- resources/views/lecturers/index.blade.php
+++ resources/views/lecturers/index.blade.php
@@ -22,6 +22,16 @@
                     </li>
                 </ul>
             </div>
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
         </div>
     </nav>
 
